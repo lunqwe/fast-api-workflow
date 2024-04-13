8591f228b97ad7d637e3f82102663987a7640c61
lunqwe <roma.boyko10068@gmail.com>
2024-04-14 00:20:28 +0300

    fixed router functions

--- routers/node.py
+++ routers/node.py
@@ -8,7 +8,7 @@ router = APIRouter()
 
 
 @router.post("/create-start-node/", tags=['nodes'])
-async def create_start_node(node_data: StartNodeSchema, db: Session = Depends(get_db)):
+def create_start_node(node_data: StartNodeSchema, db: Session = Depends(get_db)):
     created_node = NodeService.create_node(node_type='start', db=db, data=node_data)
     if created_node: # if start node doesn`t exist before (for specific workflow)`
         return created_node
@@ -16,15 +16,15 @@ async def create_start_node(node_data: StartNodeSchema, db: Session = Depends(ge
         return Response(content="Start node for this workflow is already exists.", status_code=200)
 
 @router.post("/create-message-node/", tags=['nodes'])
-async def create_message_node(node_data: MessageNodeSchema, db: Session = Depends(get_db)):
+def create_message_node(node_data: MessageNodeSchema, db: Session = Depends(get_db)):
     return NodeService.create_node(node_type='message', db=db, data=node_data)
 
 @router.post("/create-condition-node/", tags=['nodes'])
-async def create_condition_node(node_data: ConditionNodeSchema, db: Session = Depends(get_db)):
+def create_condition_node(node_data: ConditionNodeSchema, db: Session = Depends(get_db)):
     return NodeService.create_node(node_type='condition', db=db, data=node_data)
 
 @router.post("/create-end-node/", tags=['nodes'])
-async def create_end_node(node_data: EndNodeSchema, db: Session = Depends(get_db)):
+def create_end_node(node_data: EndNodeSchema, db: Session = Depends(get_db)):
     created_node = NodeService.create_node(node_type='end', db=db, data=node_data) 
     if created_node: # if end node doesn`t exist before (for specific workflow)`
         return created_node
@@ -32,28 +32,28 @@ async def create_end_node(node_data: EndNodeSchema, db: Session = Depends(get_db
         return Response(content="End node for this workflow is already exists.", status_code=200)
 
 @router.put('/update-start-node/{node_id}', tags=['nodes'])
-async def update_start_node(node_data: StartNodeSchema, node_id: int, db: Session = Depends(get_db)):
+def update_start_node(node_data: StartNodeSchema, node_id: int, db: Session = Depends(get_db)):
     return NodeService.update_node(node_id=node_id, db=db, data=node_data)
 
 @router.put('/update-message-node/{node_id}', tags=['nodes'])
-async def update_message_node(node_data: MessageNodeSchema, node_id: int, db: Session = Depends(get_db)):
+def update_message_node(node_data: MessageNodeSchema, node_id: int, db: Session = Depends(get_db)):
     return NodeService.update_node(node_id=node_id, db=db, data=node_data)
 
 @router.put('/update-condition-node/{node_id}', tags=['nodes'])
-async def update_condition_node(node_data: ConditionNodeSchema, node_id: int, db: Session = Depends(get_db)):
+def update_condition_node(node_data: ConditionNodeSchema, node_id: int, db: Session = Depends(get_db)):
     return NodeService.update_node(node_id=node_id, db=db, data=node_data)
 
 @router.put('/update-end-node/{node_id}', tags=['nodes'])
-async def update_end_node(node_data: EndNodeSchema, node_id: int, db: Session = Depends(get_db)):
+def update_end_node(node_data: EndNodeSchema, node_id: int, db: Session = Depends(get_db)):
     return NodeService.update_node(node_id=node_id, db=db, data=node_data)
 
 @router.get("/{node_id}", tags=['nodes'])
-async def read_node(node_id: int, db: Session = Depends(get_db)):
+def read_node(node_id: int, db: Session = Depends(get_db)):
     db_node = NodeService.get_node(db=db, node_id=node_id)
     if db_node is None:
         raise HTTPException(status_code=404, detail="Node not found.")
     return db_node
 
 @router.delete("/delete/{node_id}", tags=['nodes'])
-async def delete_node(node_id: int, db: Session = Depends(get_db)):
+def delete_node(node_id: int, db: Session = Depends(get_db)):
     return NodeService.delete_node(db=db, node_id=node_id)
--- routers/workflow.py
+++ routers/workflow.py
@@ -7,11 +7,11 @@ from schemas.workflow import *
 router = APIRouter()
 
 @router.post('/create-workflow', tags=['workflows'])
-async def create_workflow(data: WorkflowCreateSchema = None, db: Session = Depends(get_db)):
+def create_workflow(data: WorkflowCreateSchema = None, db: Session = Depends(get_db)):
     return WorkflowServices.create_workflow(data, db)
 
 @router.get('/get/{id}', tags=['workflows'])
-async def get_workflow(id: int = None, db: Session = Depends(get_db)):
+def get_workflow(id: int = None, db: Session = Depends(get_db)):
     workflow = WorkflowServices.get_workflow(db=db, workflow_id=id)
     if workflow:
         return workflow
@@ -19,13 +19,13 @@ async def get_workflow(id: int = None, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Workflow not found.")
 
 @router.put('/update/{id}', tags=['workflows'])
-async def update_workflow(workflow_id: int, data: WorkflowSchema, db: Session = Depends(get_db)):
+def update_workflow(workflow_id: int, data: WorkflowSchema, db: Session = Depends(get_db)):
     return WorkflowServices.update_workflow(data=data, db=db, workflow_id=workflow_id)
 
 @router.delete('/delete/{id}', tags=['workflows'])
-async def delete_workflow(workflow_id: int, db: Session = Depends(get_db)):
+def delete_workflow(workflow_id: int, db: Session = Depends(get_db)):
     return WorkflowServices.delete_workflow(db=db, workflow_id=workflow_id)
 
 @router.post('/run-sequence/{workflow_id}', tags=['workflows'])
-async def run_sequence(workflow_id: int, db: Session = Depends(get_db)):
+def run_sequence(workflow_id: int, db: Session = Depends(get_db)):
     return WorkflowServices.create_and_run_graph(db=db, workflow_id=workflow_id)
