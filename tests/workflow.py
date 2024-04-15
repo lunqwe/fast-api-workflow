15e161821ab6f827c13dbd23af5b63704a912790
lunqwe <roma.boyko10068@gmail.com>
2024-04-15 23:13:29 +0300

    dumb mistake fix

--- tests/workflow.py
+++ tests/workflow.py
@@ -5,7 +5,6 @@ from schemas.workflow import WorkflowCreateSchema, StartNodeSchema, ConditionNod
 from routers import workflow as WorkflowRoutes, node as NodeRoutes
 from sqlalchemy.orm import sessionmaker
 from database import engine
-import asyncio
 
 
 @pytest.fixture(scope="session")
@@ -18,7 +17,7 @@ def db():
 def test_create_workflow(db):
     workflow_name = "Test Workflow"
     workflow_data = WorkflowCreateSchema(name=workflow_name)
-    created_workflow = asyncio.run(WorkflowRoutes.create_workflow(data=workflow_data, db=db))
+    created_workflow = WorkflowRoutes.create_workflow(data=workflow_data, db=db)
 
     assert created_workflow.name == workflow_name
 
@@ -28,9 +27,9 @@ def test_create_workflow(db):
 def test_update_workflow(db):
     workflow_name = "Test Workflow"
     new_workflow_name = "Updated Workflow"
-    workflow_to_update = asyncio.run(WorkflowRoutes.create_workflow(data=WorkflowCreateSchema(name=workflow_name), db=db))
+    workflow_to_update = WorkflowRoutes.create_workflow(data=WorkflowCreateSchema(name=workflow_name), db=db)
 
-    updated_workflow = asyncio.run(WorkflowRoutes.update_workflow(workflow_id=workflow_to_update.id, data=WorkflowCreateSchema(name=new_workflow_name), db=db))
+    updated_workflow = WorkflowRoutes.update_workflow(workflow_id=workflow_to_update.id, data=WorkflowCreateSchema(name=new_workflow_name), db=db)
 
     assert updated_workflow.id == workflow_to_update.id
     assert updated_workflow.name == new_workflow_name
@@ -40,8 +39,8 @@ def test_update_workflow(db):
 
 def test_delete_workflow(db):
     workflow_name = "Test Workflow"
-    workflow = asyncio.run(WorkflowRoutes.create_workflow(data=WorkflowCreateSchema(name=workflow_name), db=db))
-    deleted_workflow = asyncio.run(WorkflowRoutes.delete_workflow(workflow_id=workflow.id, db=db))
+    workflow = WorkflowRoutes.create_workflow(data=WorkflowCreateSchema(name=workflow_name), db=db)
+    deleted_workflow = WorkflowRoutes.delete_workflow(workflow_id=workflow.id, db=db)
 
     assert deleted_workflow == 1
     
@@ -50,7 +49,7 @@ def test_delete_workflow(db):
 def test_create_and_run_graph(db): # Valid data 
 
     workflow_data = WorkflowCreateSchema(name='test')
-    workflow = asyncio.run(WorkflowRoutes.create_workflow(data=workflow_data, db=db))
+    workflow = WorkflowRoutes.create_workflow(data=workflow_data, db=db)
     print('id', workflow.id)
 
     # Creating nodes with no edges
@@ -61,31 +60,31 @@ def test_create_and_run_graph(db): # Valid data
     no_node_data = MessageNodeSchema(workflow_id=workflow.id, message_text='no message node', status='sent', next_node_id=0)
     end_node_data = EndNodeSchema(workflow_id=workflow.id)
 
-    start_node = asyncio.run(NodeRoutes.create_start_node(node_data=start_node_data, db=db))
-    message_node = asyncio.run(NodeRoutes.create_message_node(node_data=message_node_data, db=db))
-    condition_node = asyncio.run(NodeRoutes.create_condition_node(node_data=condition_node_data, db=db))
-    no_node = asyncio.run(NodeRoutes.create_message_node(node_data=no_node_data, db=db))
-    yes_node = asyncio.run(NodeRoutes.create_message_node(node_data=yes_node_data, db=db))
-    end_node = asyncio.run(NodeRoutes.create_end_node(node_data=end_node_data, db=db))
+    start_node = NodeRoutes.create_start_node(node_data=start_node_data, db=db)
+    message_node = NodeRoutes.create_message_node(node_data=message_node_data, db=db)
+    condition_node = NodeRoutes.create_condition_node(node_data=condition_node_data, db=db)
+    no_node = NodeRoutes.create_message_node(node_data=no_node_data, db=db)
+    yes_node = NodeRoutes.create_message_node(node_data=yes_node_data, db=db)
+    end_node = NodeRoutes.create_end_node(node_data=end_node_data, db=db)
 
     # updating nodes edges with exist nodes
     start_node_data = StartNodeSchema(workflow_id=workflow.id, next_node_id=message_node.id)
-    asyncio.run(NodeRoutes.update_start_node(node_id=start_node.id, node_data=start_node_data, db=db))
+    NodeRoutes.update_start_node(node_id=start_node.id, node_data=start_node_data, db=db)
 
     message_node_data = MessageNodeSchema(workflow_id=workflow.id, message_text='test', status='sent', next_node_id=condition_node.id)
-    asyncio.run(NodeRoutes.update_message_node(node_data=message_node_data, node_id=message_node.id, db=db))
+    NodeRoutes.update_message_node(node_data=message_node_data, node_id=message_node.id, db=db)
 
     condition_node_data = ConditionNodeSchema(workflow_id=workflow.id, yes_node_id=yes_node.id, no_node_id=no_node.id, condition="status == 'sent'")
-    asyncio.run(NodeRoutes.update_condition_node(node_data=condition_node_data, node_id=condition_node.id, db=db))
+    NodeRoutes.update_condition_node(node_data=condition_node_data, node_id=condition_node.id, db=db)
 
     yes_node_new_data = MessageNodeSchema(workflow_id=workflow.id, message_text='yes message node', status='sent', next_node_id=end_node.id)
-    asyncio.run(NodeRoutes.update_message_node(node_data=yes_node_new_data , node_id=yes_node.id, db=db))
+    NodeRoutes.update_message_node(node_data=yes_node_new_data , node_id=yes_node.id, db=db)
 
     no_node_new_data = MessageNodeSchema(workflow_id=workflow.id, message_text='no message node', status='sent', next_node_id=start_node.id)
-    asyncio.run(NodeRoutes.update_message_node(node_data=no_node_new_data , node_id=no_node.id, db=db))
+    NodeRoutes.update_message_node(node_data=no_node_new_data , node_id=no_node.id, db=db)
 
     # Running sequence
-    result = asyncio.run(WorkflowRoutes.run_sequence(workflow_id=workflow.id, db=db))
+    result = WorkflowRoutes.run_sequence(workflow_id=workflow.id, db=db)
 
     assert 'success_path' in result
     assert 'edges' in result
